5e32fe24dcef3e033c62ae2fccb9f214acada866
Konstantsin Khraneko <mr.khraneko@gmail.com>
2026-02-28 16:43:03 +0100

    Rewrited .java file for Lab1

--- Lab1/Lab1.java
+++ Lab1/Lab1.java
@@ -1,18 +1,3 @@
-class Human {
-    public String name;
-    public String surname;
-    public int yearOfBirth; 
-
-    public Human(String name, String surname, int yearOfBirth){
-        this.name = name;
-        this.surname = surname;
-        this.yearOfBirth = yearOfBirth;
-    }
-
-    @Override
-    public String toSting(){
-        return "Human: name: " + name + ", surname: " + surname + ", year of birth: " + yearOfBirth
-    }
-
-    public class
+void main (){
+    System.out.println("Hello, Vistula");
 }
